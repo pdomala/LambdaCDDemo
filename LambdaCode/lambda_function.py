6b10c42237dcee1e8aef969e65e3d8946cfa99f1
JdurgaPrasad <70344680+JdurgaPrasad@users.noreply.github.com>
2021-07-18 19:18:30 +0530

    Delete LambdaCode directory

--- LambdaCode/lambda_function.py
+++
@@ -1,7 +0,0 @@
-import boto3
-import json
-
-ec2 = boto3.client('ec2')
-def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
